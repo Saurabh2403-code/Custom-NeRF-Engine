1114bd5e6ae493a9e477663f85c3ac3493e9726e
Saurabh2403-code <girisaurabh2020@gmail.com>
2026-01-02 20:20:14 +0530

    Update nerf_config.py

--- nerf_config.py
+++ nerf_config.py
@@ -2,16 +2,15 @@ from dataclasses import dataclass
 
 @dataclass
 class NerfConfig:
-    # Training Hyperparameters
     batch_size: int = 1024
     epochs: int = 50000
     learning_rate: float = 5e-4
     
-    # Model Architecture
+
     L_pos: int = 6
     L_dir: int = 4
     
-    # Sampling Constants
+
     n_coarse: int = 64
     n_fine: int = 128
 
